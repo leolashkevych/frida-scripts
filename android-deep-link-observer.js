5bd04fcd793acb48e35eb3b89d9833ce49023944
Leo Lashkevych <leo@lashkevych.me>
2020-09-06 23:15:52 -0400

    Create android-deep-link-observer.js

---
+++ android-deep-link-observer.js
@@ -0,0 +1,23 @@
+Java.perform(function() {
+
+	var Intent = Java.use("android.content.Intent");
+	Intent.getData.implementation = function () {
+    var action = this.getAction() != null ? this.getAction().toString() : false;
+    if (action) {
+      console.log("[*] Activity: " + this.getComponent().getClassName());
+      console.log("[*] Action: " + action);
+      var uri = this.getData();
+      if(uri != null){
+        console.log("\n[*] Data");
+        uri.getScheme()   && console.log("- Scheme:\t" + uri.getScheme() + "://");
+        uri.getHost()     && console.log("- Host:\t\t/" + uri.getHost());
+        uri.getQuery()    && console.log("- Params:\t" + uri.getQuery());
+        uri.getFragment() && console.log("- Fragment:\t" + uri.getFragment());
+        console.log("\n\n");
+      }else {
+        console.log("[-] No data supplied.");
+      }
+    }
+		return this.getData();
+	}
+});
